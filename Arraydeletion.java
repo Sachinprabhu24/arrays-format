97d6318f89189a4bfeac1a5f1fd21e43ad3b6723
Sachinprabhu24 <sachinjr005@gmail.com>
2026-05-29 16:31:13 +0530

    Array deletion in java

---
+++ Arraydeletion.java
@@ -0,0 +1,12 @@
+public class Arraydeletion{
+    public static void main(String [] args){
+        int []numbers={10,20,30,40,50};
+        int deletionArray=2;
+        for (int i=deletionArray;i<numbers.length-1;i++){
+            numbers[i]=numbers[i+1];
+        }
+        for (int i=0;i<numbers.length-1;i++){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+}
